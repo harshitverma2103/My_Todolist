77b0cb7a9e7cdb8840f08e1779679ebd3ee38c67
harshitverma2103 <harshitverma2103@gmail.com>
2024-02-03 00:20:06 +0530

    add animation in add btn

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -16,17 +16,19 @@ function TodoForm() {
   }
 
   return (
-    <form onSubmit={add} className="flex">
+    <form onSubmit={add} className="flex px-3">
       <input
         type="text"
         placeholder="Write Todo..."
-        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5 hover:bg-white hover:text-black"
+        className="w-full border border-black/10 rounded-lg px-3 outline-none duration-150 bg-white/20 py-1.5 hover:bg-white hover:text-black"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
       />
-      <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0 hover:bg-green-900 ">
+      <div className='px-2'>
+      <button type="submit" className="rounded-lg px-3 py-2 bg-green-600 text-white shrink-0  hover:animate-bounce ">
         Add
       </button>
+      </div>
     </form>
   );
 }
