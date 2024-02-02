479f839db084a57cbf6bf4e7b3ae0f3bbc2e375b
harshitverma2103 <harshitverma2103@gmail.com>
2024-02-02 21:45:45 +0530

    add hover effects

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -20,11 +20,11 @@ function TodoForm() {
       <input
         type="text"
         placeholder="Write Todo..."
-        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5 hover:bg-white hover:text-black"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
       />
-      <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+      <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0 hover:bg-green-900 ">
         Add
       </button>
     </form>
